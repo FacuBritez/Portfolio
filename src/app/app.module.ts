c8c2b5cf9c64fe7394a39b20a07527875700fac8
FacuBritez <FacundoIvanBritez@gmail.com>
2023-04-16 01:50:25 -0300

    Generando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreMiComponent,
+    ProyectosComponent,
+    BannerComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
