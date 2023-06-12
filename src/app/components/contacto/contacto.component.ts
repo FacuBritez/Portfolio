bcf188ecd34f2eb7ca1a660a0e83ce291f0e3d82
FacuBritez <FacundoIvanBritez@gmail.com>
2023-06-12 07:48:48 -0300

    Agregando boton para copiar al portapapeles

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.sass']
 })
 export class ContactoComponent {
-
+  copyContent() {
+    const emailElement = document.getElementById('mail') as HTMLParagraphElement;
+    const email = emailElement.innerText;
+    navigator.clipboard.writeText(email).then(() => {
+      console.log('Contenido copiado al portapapeles');
+    }).catch((error) => {
+      console.error('Error al copiar contenido al portapapeles:', error);
+    });
+  }
 }
